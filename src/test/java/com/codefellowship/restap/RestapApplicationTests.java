20a52b3d201b97901ccb4e94f23c62f0b82501df
naeemalomari <naeemalomari96@gmail.com>
2021-10-17 20:27:18 +0300

    spring indhvcejyvce

---
+++ src/test/java/com/codefellowship/restap/RestapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefellowship.restap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
